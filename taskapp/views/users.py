f9d063ba2c95fcb836437fbba60b2e5510e75d47
anoop <none@none>
2010-02-04 22:14:34 +0530

    added views, templates for register, login, logout user.

--- taskapp/views/users.py
+++ taskapp/views/users.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render_to_response
 from pytask.taskapp.models import Task
+from pytask.taskapp.forms.user import RegistrationForm, LoginForm
+from pytask.taskapp.events.user import createUser
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 def redirect_to_homepage(request):
     """ simply redirect to homepage """
@@ -37,5 +41,52 @@ def homepage(request):
                }
                
     return render_to_response('index.html', context)
-               
 
+
+def register(request):
+    """ user registration: gets the user details and create the user and userprofile if data entered is valid"""
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            if data['password'] == data['repeat_password']:
+                try:
+                    if User.objects.get(username__exact = data['username']):
+                        errors=['Choose some other username']
+                        return render_to_response('user/register.html',{'form':form,'errors':errors})
+                except:
+                     u = createUser(username=data['username'], email=data['email'], password=data['password'],dob = data['dob'],gender = data['gender'])
+                return redirect('/accounts/login/')
+            else:
+                errors=['Password do not match']
+                form = RegistrationForm(request.POST)
+                return render_to_response('user/register.html',{'form':form,'errors':errors})#HttpResponse('Password did not match')
+        else:
+            form = RegistrationForm()
+    else:
+        form = RegistrationForm()
+    return render_to_response('user/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('/')# Redirect to a success page.
+            else:
+                return HttpResponse('username is not active, please contact the administrator')# Return a 'disabled account' error message
+        else:
+            errors = ['Please check your username and password']
+            form = LoginForm()
+            return render_to_response('user/login.html',{'form':form,'errors':errors})# Return an 'invalid login' error message.
+        return redirect('/')
+    else:
+        form = LoginForm()
+        return render_to_response('user/login.html',{'form': form})
+
+def user_logout(request):
+    logout(request)
+    return HttpResponse('You have logged off successfully!!!')
--- urls.py
+++ urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-from pytask.taskapp.views.users import redirect_to_homepage, homepage
+from pytask.taskapp.views.users import redirect_to_homepage, homepage, register, user_login, user_logout
 from pytask.taskapp.views.tasks import browse_tasks, view_task
 
 urlpatterns = patterns('',
@@ -20,5 +20,8 @@ urlpatterns = patterns('',
     (r'^task/view/tid=(\d+)', view_task),
     
     (r'^admin/', include(admin.site.urls)),
-
+    
+    (r'^accounts/register/$',register),
+    (r'^accounts/login/$',user_login),
+    (r'^accounts/logout/$',user_logout)
 )
