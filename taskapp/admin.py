3ed1f252c1d278fe306f533742d4016f5d3ba49e
nishanth <none@none>
2010-02-04 22:06:22 +0530

    corrected admin.py

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 
-from pytask.taskapp.models.user import Profile
-from pytask.taskapp.models.task import Task
-from pytask.taskapp.models.credit import Credit
-from pytask.taskapp.models.comment import Comment
+from pytask.taskapp.models import Profile, Task, Credit, Comment
 
 admin.site.register(Profile)
 admin.site.register(Task)
