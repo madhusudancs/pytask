f7d1d873d68a6c25e6aeb372881d0b700e812a9e
Nishanth Amuluru <nishanth@fossee.in>
2011-01-06 17:11:56 +0530

    Created notification and request models.

--- profile/models.py
+++ profile/models.py
@@ -9,8 +9,16 @@ RIGHTS_CHOICES = (
 	("MG", "Manager"),
 	("CT", "Contributor"),)
 
+ROLE_CHOICES = (
+	("DC", "Request sent by Director \
+                to a user at lower level, asking him to act as a director"),
+	("MG", "Request sent by Manager \
+                to a user at lower level, asking him to act as a manager"),
+
 class Profile(models.Model):
     
+    uniq_key = models.CharField(max_length=20)
+
     user = models.ForeignKey(User, unique = True)
     rights = models.CharField(max_length = 2, choices = RIGHTS_CHOICES, default = u"CT")
     pynts = models.PositiveSmallIntegerField(default = 0)
@@ -27,3 +35,41 @@ class Profile(models.Model):
 
     def __unicode__(self):
         return unicode(self.user.username)
+
+class Notification(models.Model):
+    """ A model to hold notifications.
+    All these are sent by the site to users.
+    Hence there is no sent_from option.
+    """
+
+    uniq_key = models.CharField(max_length=20)
+
+    sent_to = models.ForeignKey(User, related_name = "%(class)s_sent_to", blank = False)
+    sent_from = models.ForeignKey(User, related_name = "%(class)s_sent_from", null = True, blank = True)
+
+    subject = models.CharField(max_length=100, blank=True)
+    message = models.TextField()
+
+    sent_date = models.DateTimeField()
+    is_read = models.BooleanField(default = False)
+    is_deleted = models.BooleanField(default = False)
+
+class RoleRequest(models.Model):
+    """ A request sent by one user to the other.
+    Typically requesting to raise one's status.
+    """
+
+    uniq_key = models.CharField(max_length=20)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES)
+    is_accepted = models.BooleanField(default=False)
+
+    message = models.TextField()
+    response = models.TextField()
+
+    sent_to = models.ForeignKey(User, related_name = "%(class)s_sent_to", blank = False)
+    sent_from = models.ForeignKey(User, related_name = "%(class)s_sent_from", null = True, blank = True)
+
+    sent_date = models.DateTimeField()
+    is_read = models.BooleanField(default = False)
+    is_deleted = models.BooleanField(default = False)
+
