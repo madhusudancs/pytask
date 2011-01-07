ee9ca4578cda64df058e768de5258426a970b2fb
Nishanth Amuluru <nishanth@fossee.in>
2011-01-07 15:16:29 +0530

    created a tag and used it in the template

---
+++ profile/templatetags/user_tags.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def notf_dsp(user):
+
+    notf_cnt = user.notification_sent_to.filter(is_deleted=False,
+                                                is_read=False).count()
+
+    return u'notifications(%s)'%notf_cnt if notf_cnt else u'notifications'
+
