d946a9b39cdddcbb565ac3926f2c5d35f9fcb05a
Nishanth Amuluru <nishanth@fossee.in>
2011-01-07 13:46:38 +0530

    changed the static serving url settings

--- settings.py
+++ settings.py
@@ -45,12 +45,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = './media/'
+MEDIA_ROOT = './static/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
