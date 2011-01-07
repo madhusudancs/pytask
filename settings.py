981f17a95a00172a0780e3926363f0ed02ab033b
Nishanth Amuluru <nishanth@fossee.in>
2011-01-07 11:48:33 +0530

    enabled admin interface

--- settings.py
+++ settings.py
@@ -94,7 +94,7 @@ INSTALLED_APPS = (
     'tagging',
     'pytask.profile',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
