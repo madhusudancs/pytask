f5b850e8b53df61b8f0d20e215543e5ddc595c1d
Nishanth Amuluru <nishanth@fossee.in>
2011-01-07 09:57:52 +0530

    added the url for creating a login

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
+from registration.views import register
+from pytask.profile.forms import CustomRegistrationForm
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -14,4 +17,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
     url(r'localhost:8000', None, name="site"),
+
+    url(r'^accounts/register/$', register,
+        {'form_class': CustomRegistrationForm},
+        name='registration_register'),
 )
