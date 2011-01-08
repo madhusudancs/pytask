3fe830ab494288ca1f3199bdd910ba2360f8661d
Nishanth Amuluru <nishanth@fossee.in>
2011-01-09 00:11:56 +0530

    Added task view url

--- pytask/taskapp/urls.py
+++ pytask/taskapp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
 
-from pytask.taskapp.views import create_task
+from pytask.taskapp.views import create_task, view_task
 
 urlpatterns = patterns('',
 
             (r'^create/$', create_task),
+            (r'^view/tid=(\w+)', view_task),
 )
 
