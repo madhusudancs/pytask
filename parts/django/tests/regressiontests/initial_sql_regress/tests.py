b03203c8cb991c16ac8a3d74c8c4078182d0bb48
Nishanth Amuluru <nishanth@fossee.in>
2011-01-11 22:41:51 +0530

    removed all the buildout files

--- parts/django/tests/regressiontests/initial_sql_regress/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-from models import Simple
-
-
-class InitialSQLTests(TestCase):
-    def test_initial_sql(self):
-        self.assertEqual(Simple.objects.count(), 7)
