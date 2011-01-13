6ebb00197b5283041fc61ff5f7ad552d4a9acad9
Madhusudan.C.S <madhusudancs@gmail.com>
2011-01-14 02:10:53 +0530

    Move local settings specific stuff out of settings.py to local.py.
    Due to security reasons local.py is not tracked by revision control system.

--- pytask/settings.py
+++ pytask/settings.py
@@ -1,24 +1,12 @@
 # Django settings for pytask project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+from pytask.local import *
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('Madhusudan C.S.', 'madhusudancs@fossee.in'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './pytask/db.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
