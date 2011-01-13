bb35c0a7546440247dad47f57d748259d0e9a744
Madhusudan.C.S <madhusudancs@gmail.com>
2011-01-14 00:59:09 +0530

    Purging the repository for the new set of changes by Nishanth.

--- taskapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from pytask.taskapp.models import Profile, Task, Comment, Notification, Request
-
-admin.site.register(Profile)
-admin.site.register(Task)
-admin.site.register(Comment)
-admin.site.register(Notification)
-admin.site.register(Request)
--- taskapp/utilities/helper.py
+++
@@ -1,8 +0,0 @@
-import string,random
-
-def get_key():
-    """ return a 10 character random key.
-    """
-    
-    return ''.join([ random.choice(string.uppercase+string.digits) for i in range(10)])
-
