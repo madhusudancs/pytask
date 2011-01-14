5082dabe477b8cdb96a9612bbefbc9f4b774cb33
Madhusudan.C.S <madhusudancs@gmail.com>
2011-01-15 03:58:06 +0530

    Add name for each URL definition.

--- pytask/profile/urls.py
+++ pytask/profile/urls.py
@@ -3,11 +3,16 @@ from django.conf.urls.defaults import url
 
 
 urlpatterns = patterns('pytask.profile.views',
-            url(r'^view/$', 'view_profile'),
-            url(r'^edit/$', 'edit_profile'),
-            url(r'^notf/browse/$', 'browse_notifications'),
-            url(r'^notf/view/nid=(\w+)$', 'view_notification'),
-            url(r'^notf/del/nid=(\w+)$', 'delete_notification'),
-            url(r'^notf/unr/nid=(\w+)$', 'unread_notification'),
-            url(r'^user/view/uid=(\w+)$', 'view_user'),
+  url(r'^view/$', 'view_profile', name='view_profile'),
+  url(r'^edit/$', 'edit_profile', name='edit_profile'),
+  url(r'^notf/browse/$', 'browse_notifications',
+      name='edit_profile'),
+  url(r'^notf/view/(?P<notification_id>\d+)$', 'view_notification',
+      name='view_notification'),
+  url(r'^notf/del/(?P<notification_id>\d+)$', 'delete_notification',
+      name='delete_notification'),
+  url(r'^notf/unr/(?P<notification_id>\d+)$', 'unread_notification',
+      name='unread_notification'),
+  url(r'^user/view/(?P<user_id>\d+)$', 'view_user',
+      name='view_user'),
 )
