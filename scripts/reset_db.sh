fd71cf3cf18c33d80db9c95431e140ee0a8533ea
Madhusudan.C.S <madhusudancs@gmail.com>
2011-01-15 21:21:19 +0530

    Remove initial south migrations also during reset of the database.

--- scripts/reset_db.sh
+++ scripts/reset_db.sh
@@ -2,6 +2,10 @@
 
 sudo -u postgres dropdb pytask
 sudo -u postgres createdb -O pytask pytask
+rm -r pytask/profile/migrations/
+rm -r pytask/taskapp/migrations/
+./bin/django schemamigration profile --initial
+./bin/django schemamigration taskapp --initial
 ./bin/django syncdb
 ./bin/django migrate profile
 ./bin/django migrate taskapp
