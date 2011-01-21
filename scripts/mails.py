3193526f131d059333a6bad34c73ed11babd711e
Madhusudan.C.S <madhusudancs@gmail.com>
2011-01-21 19:57:40 +0530

    Fixed the getting profile and name model property.

--- scripts/mails.py
+++ scripts/mails.py
@@ -18,12 +18,12 @@ def textbook_workshop_remainder(subject_template=None, body_template=None):
 
     users = User.objects.all()
 
-    subject = loader.render_to_string(subject_template)
+    subject = loader.render_to_string(subject_template).strip(' \n\t')
 
     for user in users:
-        profile = user.getprofile()
+        profile = user.get_profile()
         if profile:
-            full_name = profile.name
+            full_name = profile.full_name
         else:
             full_name = ''
 
