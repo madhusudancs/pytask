2ae70bc48ab4bf70e0230fad6f378a962c4dc4d0
Madhusudan.C.S <madhusudancs@gmail.com>
2011-02-11 12:04:29 +0530

    Add copyright notice to all the files.

--- pytask/taskapp/views/task.py
+++ pytask/taskapp/views/task.py
@@ -1,3 +1,23 @@
+#!/usr/bin/env python
+#
+# Copyright 2011 Authors of PyTask.
+#
+# This file is part of PyTask.
+#
+# PyTask is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyTask is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyTask.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """Module containing the views for all the task related activities.
 
 If there is something common to all other views. They sit here, here
@@ -7,8 +27,8 @@ for everything else.
 
 
 __authors__ = [
-    '"Nishanth Amuluru" <nishanth@fossee.in>',
     '"Madhusudan.C.S" <madhusudancs@fossee.in>',
+    '"Nishanth Amuluru" <nishanth@fossee.in>',
     ]
 
 
--- scripts/profile_schemamigration.sh
+++ scripts/profile_schemamigration.sh
@@ -1,4 +1,26 @@
 #! /bin/bash
+#
+# Copyright 2011 Authors of PyTask.
+#
+# This file is part of PyTask.
+#
+# PyTask is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyTask is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyTask.  If not, see <http://www.gnu.org/licenses/>.
+#
+# authors__ = [
+#     '"Madhusudan.C.S" <madhusudancs@fossee.in>',
+#     ]
+
 
 pg_dump pytask -U pytask -W > ~/pytaskdbdumps/dbdump`date +%Y%m%d%H%M%s`.dump
 ./bin/django dumpscript > ~/pytaskdbdumps/dbdumpscript`date +%Y%m%d%H%M%s`.dump
